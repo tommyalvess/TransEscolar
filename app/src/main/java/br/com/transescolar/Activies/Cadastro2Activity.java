e1a3c568d34d10e14c02a18abbc3bca052bfcef3
Tom Alves <eliton.alves_santos@hotmail.com>
2018-07-22 16:51:35 -0300

    Login e Cadastro OK

---
+++ app/src/main/java/br/com/transescolar/Activies/Cadastro2Activity.java
@@ -0,0 +1,15 @@
+package br.com.transescolar.Activies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.transescolar.R;
+
+public class Cadastro2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro2);
+    }
+}
